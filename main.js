5211a7b689ba4601efc4d6bcf9d9cfcbe59fa388
labstraction <labstraction@protonmail.com>
2025-03-06 09:56:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+//rgbString, hex, array, object
+
+function randomColorArray(){
+
+
+}
+
+function randomColorHex(){
+
+}
+
+
+function randomColorRgb(){
+
+}
+
+
+function randomColorObj(){
+
+}
+
+
+console.log(randomColorArray())
+console.log(randomColorHex())
+console.log(randomColorRgb())
+console.log(randomColorObj())
